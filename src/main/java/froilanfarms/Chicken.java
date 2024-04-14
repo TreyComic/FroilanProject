68365bc80a01a3f7522f360829e10e67c0534c30
KalilK13 <kalilkennedy13@gmail.com>
2024-04-13 20:24:20 -0400

    Getters and setters for hasBeenFertilized.

--- src/main/java/froilanfarms/Chicken.java
+++ src/main/java/froilanfarms/Chicken.java
@@ -4,8 +4,17 @@ public class Chicken extends Animal implements Produce{
     private boolean hasBeenFertilized;
     private static int id = 1;
     private int uniqueID;
-    public Chicken(){
+    public Chicken(boolean fertilized){
+        setHasBeenFertilized(fertilized);
         uniqueID = id++;
+    }
+
+    public boolean isHasBeenFertilized() {
+        return hasBeenFertilized;
+    }
+
+    public void setHasBeenFertilized(boolean hasBeenFertilized) {
+        this.hasBeenFertilized = hasBeenFertilized;
     }
 
     public void yield(){}
