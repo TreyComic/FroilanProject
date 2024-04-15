6aa64d3034e09a1d8656d187023030217d0daf88
elcorkum <147072591+elcorkum@users.noreply.github.com>
2024-04-15 17:40:45 -0400

    Update Pilot.java

--- src/main/java/froilanfarms/Pilot.java
+++ src/main/java/froilanfarms/Pilot.java
@@ -1,15 +1,13 @@
 package froilanfarms;
 
 public class Pilot extends Person{
-    public Pilot(CropDuster cropDuster){
-    }
     @Override
     public void eat() {
-
+        System.out.println("Pilot is eating");    
     }
 
     @Override
     public void makeNoise() {
-
+        System.out.println("Pilot is talking.");
     }
 }
